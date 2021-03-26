662c654c6b234505d64bea8ecf5da8d07483404c
Sergey Bronnikov <estetus@gmail.com>
2021-03-26 17:47:40 +0300

    backends: add test for gitlab backend

--- backends/backend_gitlab_test.go
+++ backends/backend_gitlab_test.go
@@ -1,9 +1,24 @@
-// https://github.com/drone/drone-go/blob/master/drone/client_test.go
-// https://github.com/ktrysmt/go-bitbucket/blob/master/tests/repository_test.go
 package backends
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestSyncGitLab(t *testing.T) {
-	t.Log("TestSyncGitLab")
+	t.Log("Basic test with cmocka/cmocka project")
+	username := os.Getenv("GITLAB_USERNAME")
+	token := os.Getenv("GITLAB_TOKEN")
+	if username == "" || token == "" {
+		t.Skip("No GITLAB_USERNAME and GITLAB_TOKEN.")
+	}
+	backend := Backend{Type: "gitlab", Base: "https://gitlab.com/", Name: "cmocka/cmocka",
+		Project: "cmocka/cmocka", Branch: "master", Username: username, Secret: token}
+
+	buildsNumber := 5
+	httpClient := NewAPIClient()
+	builds, err := SyncGitLab(httpClient, &backend, buildsNumber)
+	if builds == nil || err != nil {
+		t.Failed()
+	}
 }
